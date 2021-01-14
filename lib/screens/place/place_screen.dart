d21b00cc6b153a58f7761b95ee80ef9bc10c672e
Dongheon, Kim <oueya1479@naver.com>
2021-01-14 17:49:04 +0900

    Divided components each dart file

--- lib/screens/place/place_screen.dart
+++ lib/screens/place/place_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:roadize/constants.dart';
 import 'package:roadize/size_config.dart';
+import 'components/place_image.dart';
+import 'components/place_information.dart';
+import 'components/place_title.dart';
 
 class PlaceScreen extends StatefulWidget {
   @override
@@ -8,95 +11,18 @@ class PlaceScreen extends StatefulWidget {
 }
 
 class _PlaceScreenState extends State<PlaceScreen> {
-  int currentPage = 0;
-
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              width: SizeConfig.screenWidth,
-              height: SizeConfig.screenHeight * 0.4,
-              child: PageView(
-                onPageChanged: (value) {
-                  setState(() {
-                    currentPage = value;
-                  });
-                },
-                children: [
-                  ImageCard(
-                    imageName: '1',
-                  ),
-                  ImageCard(
-                    imageName: '2',
-                  ),
-                  ImageCard(
-                    imageName: '3',
-                  ),
-                  ImageCard(
-                    imageName: '4',
-                  ),
-                  ImageCard(
-                    imageName: '1',
-                  ),
-                  ImageCard(
-                    imageName: '2',
-                  ),
-                  ImageCard(
-                    imageName: '3',
-                  ),
-                  ImageCard(
-                    imageName: '4',
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-                bottom: kDefaultPadding * 0.5,
-                left: 0,
-                right: 0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    10,
-                    (index) => Container(
-                      width: currentPage == index
-                          ? SizeConfig.screenWidth * 0.05
-                          : SizeConfig.screenWidth * 0.03,
-                      height: currentPage == index
-                          ? SizeConfig.screenWidth * 0.015
-                          : SizeConfig.screenWidth * 0.006,
-                      decoration: BoxDecoration(
-                        color: currentPage == index
-                            ? kSecondLightColor
-                            : Colors.white,
-                      ),
-                    ),
-                  ),
-                ))
-          ],
-        ),
-      ),
+          body: Column(
+        children: [
+          PlaceImage(),
+          PlaceTitle(),
+          PlaceInformation(),
+        ],
+      )),
     );
-  }
-}
-
-class ImageCard extends StatelessWidget {
-  ImageCard({this.imageName});
-  final imageName;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: SizeConfig.screenWidth,
-        height: SizeConfig.screenHeight * 0.4,
-        decoration: BoxDecoration(
-            color: Colors.black,
-            image: DecorationImage(
-                image: AssetImage('images/$imageName.jpg'),
-                fit: BoxFit.cover)));
   }
 }
