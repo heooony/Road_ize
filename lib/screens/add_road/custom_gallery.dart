95de8f8530e3b18de349ea0ac86829e38c4f05f7
Dongheon, Kim <oueya1479@naver.com>
2021-01-25 23:21:36 +0900

    Remove draggable container image

--- lib/screens/add_road/custom_gallery.dart
+++ lib/screens/add_road/custom_gallery.dart
@@ -15,47 +15,16 @@ class CustomGallery extends StatefulWidget {
 class CustomGalleryState extends State<CustomGallery> {
   static Future<File> imageFile;
   GridGallery gridGallery;
-  double _zoom;
-  double _previousZoom;
-  Offset _previousOffset;
-  Offset _offset;
-  Offset _position;
-  File _image;
 
   @override
   void initState() {
     super.initState();
-    _zoom = 1.0;
-    _previousZoom = null;
-    _offset = Offset.zero;
-    _position = Offset(0, 0);
     gridGallery = GridGallery(this.callback);
   }
 
   void callback(Future<File> image) {
     setState(() {
       imageFile = image;
-    });
-  }
-
-  void _handleScaleStart(ScaleStartDetails start) {
-    setState(() {
-      _previousOffset = _offset;
-      _previousZoom = _zoom;
-    });
-  }
-
-  void _handleScaleUpdate(ScaleUpdateDetails update) {
-    setState(() {
-      _zoom = _previousZoom * update.scale;
-    });
-  }
-
-  void _handleScaleReset() {
-    setState(() {
-      _zoom = 1.0;
-      _offset = Offset.zero;
-      _position = Offset.zero;
     });
   }
 
@@ -73,44 +42,11 @@ class CustomGalleryState extends State<CustomGallery> {
           return Column(
             children: [
               Expanded(
-                flex: 4,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      left: _position.dx,
-                      top: _position.dy,
-                      child: Draggable(
-                        child: Container(
-                          padding: const EdgeInsets.all(10.0),
-                          width: 350.0,
-                          height: 450.0,
-                          child: GestureDetector(
-                            onScaleStart: _handleScaleStart,
-                            onScaleUpdate: _handleScaleUpdate,
-                            onDoubleTap: _handleScaleReset,
-                            child: Transform(
-                              transform: Matrix4.diagonal3(
-                                  Vector3(_zoom, _zoom, _zoom)),
-                              alignment: FractionalOffset.center,
-                              child: Image.file(file),
-                            ),
-                          ),
-                        ),
-                        onDraggableCanceled: (velocity, offset) {
-                          setState(() {
-                            _position = offset;
-                          });
-                        },
-                        feedback: Container(
-                          width: 1.0,
-                          height: 1.0,
-                          child: Image.file(file),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+                  flex: 4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(image: FileImage(file))),
+                  )),
               DividedBar(),
               gridGallery,
             ],
