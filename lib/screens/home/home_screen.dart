29ee84888489b0412a2db842f4288f4606da132c
Dongheon, Kim <oueya1479@naver.com>
2021-01-26 20:54:40 +0900

    Add main text

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -14,194 +14,203 @@ class _HomeScreenState extends State<HomeScreen> {
     final size = MediaQuery.of(context).size;
     SizeConfig.init(context);
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(
-          getProportionateScreenWidth(20.0),
-        ),
-        child: ListWheelScrollView(
-          itemExtent: SizeConfig.screenHeight * 0.65,
-          physics: FixedExtentScrollPhysics(),
-          magnification: 1.0,
-          perspective: 0.002,
-          overAndUnderCenterOpacity: 0.5,
-          diameterRatio: 4.0,
-          children: [
-            Stack(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(
+              getProportionateScreenWidth(20.0),
+            ),
+            child: ListWheelScrollView(
+              itemExtent: SizeConfig.screenHeight * 0.65,
+              physics: FixedExtentScrollPhysics(),
+              magnification: 1.0,
+              perspective: 0.002,
+              overAndUnderCenterOpacity: 0.5,
+              diameterRatio: 4.0,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                      image: AssetImage('images/1.jpg'),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5), BlendMode.dstIn),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage('images/1.jpg'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.5), BlendMode.dstIn),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        right: kDefaultPadding,
-                        left: kDefaultPadding,
-                        bottom: kDefaultPadding * 2),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '하늘이 맑을 때 가기 좋은 곳',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: SizeConfig.fontSize * 1.3),
-                        ),
-                        Text(
-                          'Designed by PotDongheon',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: SizeConfig.fontSize),
-                        ),
-                        Row(
+                    Positioned(
+                      right: 0,
+                      left: 0,
+                      bottom: 0,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            right: kDefaultPadding,
+                            left: kDefaultPadding,
+                            bottom: kDefaultPadding * 2),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(
-                              Icons.location_on_rounded,
-                              color: kSecondColor,
-                            ),
                             Text(
-                              '15 Road',
+                              '하늘이 맑을 때 가기 좋은 곳',
                               style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: SizeConfig.fontSize * 0.8),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: SizeConfig.fontSize * 1.3),
+                            ),
+                            Text(
+                              'Designed by PotDongheon',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: SizeConfig.fontSize),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_rounded,
+                                  color: kSecondColor,
+                                ),
+                                Text(
+                                  '15 Road',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: SizeConfig.fontSize * 0.8),
+                                )
+                              ],
                             )
                           ],
-                        )
-                      ],
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage('images/1.jpg'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.5), BlendMode.dstIn),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      left: 0,
+                      bottom: 0,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            right: kDefaultPadding,
+                            left: kDefaultPadding,
+                            bottom: kDefaultPadding * 2),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '하늘이 맑을 때 가기 좋은 곳',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: SizeConfig.fontSize * 1.3),
+                            ),
+                            Text(
+                              'Designed by PotDongheon',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: SizeConfig.fontSize),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_rounded,
+                                  color: kSecondColor,
+                                ),
+                                Text(
+                                  '15 Road',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: SizeConfig.fontSize * 0.8),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage('images/1.jpg'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(0.5), BlendMode.dstIn),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      left: 0,
+                      bottom: 0,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            right: kDefaultPadding,
+                            left: kDefaultPadding,
+                            bottom: kDefaultPadding * 2),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '타이틀',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: SizeConfig.fontSize * 1.3),
+                            ),
+                            Text(
+                              'Designed by PotDongheon',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: SizeConfig.fontSize),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_rounded,
+                                  color: kSecondColor,
+                                ),
+                                Text(
+                                  '15 Road',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: SizeConfig.fontSize * 0.8),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                      image: AssetImage('images/1.jpg'),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5), BlendMode.dstIn),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        right: kDefaultPadding,
-                        left: kDefaultPadding,
-                        bottom: kDefaultPadding * 2),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '하늘이 맑을 때 가기 좋은 곳',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: SizeConfig.fontSize * 1.3),
-                        ),
-                        Text(
-                          'Designed by PotDongheon',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: SizeConfig.fontSize),
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on_rounded,
-                              color: kSecondColor,
-                            ),
-                            Text(
-                              '15 Road',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: SizeConfig.fontSize * 0.8),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                      image: AssetImage('images/1.jpg'),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.5), BlendMode.dstIn),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                        right: kDefaultPadding,
-                        left: kDefaultPadding,
-                        bottom: kDefaultPadding * 2),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: SizeConfig.fontSize * 1.3),
-                        ),
-                        Text(
-                          'Designed by PotDongheon',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: SizeConfig.fontSize),
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.location_on_rounded,
-                              color: kSecondColor,
-                            ),
-                            Text(
-                              '15 Road',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: SizeConfig.fontSize * 0.8),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+          Text(
+            'MAIN 창입니다~~',
+            style:
+                TextStyle(fontSize: SizeConfig.fontSize, color: Colors.black),
+          ),
+        ],
       ),
     );
   }
